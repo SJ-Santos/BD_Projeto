1e38192ce9986dee5fe5b3a6f201b2907fe6a3cd
Evellyn Buonafina <rian@agenciakerno.com.br>
2025-05-12 09:28:44 -0300

    create view

---
+++ App/Views/view.py
@@ -0,0 +1,9 @@
+def view_admin(cursor):
+  cursor.execute("CREATE VIEW Administrador AS" \
+  "SELECT ")
+
+def view_gerente(cursor):
+  cursor.execute("CREATE VIEW Gerente AS")
+
+def view_func(cursor):
+  cursor.execute("CREATE VIEW Funcionario AS")
