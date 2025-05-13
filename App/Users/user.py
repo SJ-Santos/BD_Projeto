cf03f1573e996bac6b54436ca2162205ee5fb382
Samuelj-santos <samuel.00000847429@unicap.br>
2025-05-12 21:26:18 -0300

    novos metodos

--- App/Users/user.py
+++ App/Users/user.py
@@ -1,3 +1,3 @@
 def create_user(cursor,user):
-    cursor.execute(f"CREATE USER '{user['username']}'@'{user['host']}' IDENTIFIED BY '{user['password']}' ")
+    cursor.execute(f"CREATE USER '{user['username']}'@'{user['host']}' IDENTIFIED BY '{user['password']}' ;")
 
