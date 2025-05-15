1d37e5669e2ff5d2459f6e3070653460c76f55da
Evellyn Buonafina <rian@agenciakerno.com.br>
2025-05-14 21:21:16 -0300

    ajustes nas views

--- App/Views/view.py
+++ App/Views/view.py
@@ -1,46 +1,42 @@
-def view_admin(cursor):
-  cursor.execute("DROP VIEW IF EXISTS admin_view")
-  cursor.execute("""CREATE VIEW admin_view" AS
+def view_prod_mais_vendidos(cursor):
+  cursor.execute("DROP VIEW IF EXISTS produtos_mais_vendidos")
+  cursor.execute("""CREATE VIEW produtos_mais_vendidos AS
   SELECT 
-      v.id AS id_venda,     
-      f.nome AS nome_funcionario,
       p.nome AS produto,
-      p.valor,
-      v.data
-      FROM venda v
-      JOIN funcionario f ON v.id_vendedor = f.id
-      JOIN cliente c ON v.id_cliente = c.id
-      JOIN produto p ON v.id = p.id
-
+      COUNT(v.id) AS total_vendido
+      FROM produto p
+      JOIN venda v ON p.id = v.id_produto
+      GROUP BY p.id, p.nome
+      ORDER BY total_vendido DESC;
   """)
+
   cursor.execute("GRANT ALL PRIVILEGES ON empresa.* TO 'admin_user'@'localhost';")
 
-def view_gerente(cursor):
-  cursor.execute("DROP VIEW IF EXISTS gerente_view")
-  cursor.execute("""CREATE VIEW gerente_view AS
+def view_resumo_vendas(cursor):
+  cursor.execute("DROP VIEW IF EXISTS resumo_vendas_funcionario")
+  cursor.execute("""CREATE VIEW resumo_vendas_funcionario AS
   SELECT 
       f.nome AS funcionario,
-      f.cargo,
-      COUNT(v.id) AS total_vendas,
-      SUM(p.valor) AS total_valor_vendido
-      FROM venda v
-      JOIN funcionario f ON v.id_vendedor = f.id
-      JOIN produto p ON v.id = p.id
-      GROUP BY f.nome, f.cargo
-
-                 
-                 
+      COUNT(v.id) AS quantidade_vendas,
+      f.salario
+      FROM funcionario f
+      JOIN venda v ON f.id = v.id_vendedor
+      GROUP BY f.id, f.nome, f.salario;            
   """)
 
-def view_func(cursor):
-  cursor.execute("DROP VIEW IF EXISTS func_view")
-  cursor.execute("""CREATE VIEW func_view AS
+def view_func_por_venda(cursor):
+  cursor.execute("DROP VIEW IF EXISTS view_func_por_venda")
+  cursor.execute("""CREATE VIEW view_func_por_venda AS
   SELECT 
-      v.id AS id_venda,
-      v.id_cliente,
-      v.id_vendedor,
-      v.data
-      FROM venda v         
+      f.nome AS funcionario,
+       c.nome AS cliente,
+       v.data,
+       v.valor
+       FROM venda v
+       JOIN funcionario f ON v.id_vendedor = f.id
+       JOIN cliente c ON v.id_cliente = c.id
+       ORDER BY v.data, v.valor DESC;
+      
   """)
 
 
